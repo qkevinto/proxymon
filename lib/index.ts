4a34e7fa709d4e860d5f22da2e471c3b0643a8d5
Kevin To <qkevinto@gmail.com>
2023-04-02 00:49:53 +1100

    Clean up some variable naming

--- lib/index.ts
+++ lib/index.ts
@@ -8,12 +8,12 @@ console.log('Proxymon: Pokémon TCG Proxy Deck Generator\n')
 async function main() {
     try {
         const rawDecklist = await fs.promises.readFile('./decklist', 'utf8')
-        const tcgDecklist = await decklistParser(rawDecklist)
-        const groupedCards: Card[] = tcgDecklist.cards
+        const decklist = await decklistParser(rawDecklist)
+        const cards: Card[] = decklist.cards
             .map(card => [card.amount, card.ptcgoio.id, card.name])
         const rawConfiguration = await fs.promises.readFile('./config.json', 'utf8')
         const configuration = JSON.parse(rawConfiguration)
-        await generate(configuration, groupedCards)
+        await generate(configuration, cards)
     } catch (err) {
         console.error(err)
     }
