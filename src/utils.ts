d99e408ce644809710c74e45ee41f7e2fe960d1d
Kevin To <qkevinto@gmail.com>
2023-04-03 21:17:26 +1000

    Add caching and load cards sequentially
    Try not to get throttled by the server by loading potentially hundreds
    of cards at the same time

--- src/utils.ts
+++ src/utils.ts
@@ -2,19 +2,50 @@ import fetch from 'node-fetch'
 import ProgressBar from 'progress'
 import { Card } from './models/card'
 import { SetCodes } from './models/set-codes'
-import { PokemonTCGAPIRepository } from './repository/pokemonTCGAPI'
+import { CardDto, PokemonTCGAPIRepository } from './repository/pokemonTCGAPI'
+
+const cardCache: { [id: string]: CardDto } = {}
+const imageCache: { [url: string]: Buffer } = {}
 
 export function mm(value: number): number {
     return value * 2.834645669
 }
 
+async function getCardWithCache(pokemonTCGAPIRepository: PokemonTCGAPIRepository, id: string): Promise<CardDto> {
+    const cached = cardCache[id]
+    if (!!cached) {
+        return Promise.resolve(cached)
+    } else {
+        const card = await pokemonTCGAPIRepository.card(id)
+        if (!card) {
+            throw Error(`Error not loading ${id}`)
+        }
+        cardCache[id] = card
+        return card
+    }
+}
+
+async function getImageWithCache(imageUrl: string): Promise<Buffer> {
+    const cached = imageCache[imageUrl]
+    if (!!cached) {
+        return Promise.resolve(cached)
+    } else {
+        const image = await fetch(imageUrl)
+        if (!image) {
+            throw Error(`Error loading ${imageUrl}`)
+        }
+        const bufferArray = await image.arrayBuffer()
+        const buffer = Buffer.from(bufferArray)
+        imageCache[imageUrl] = buffer
+        return buffer
+    }
+}
+
 async function getCardImage(pokemonTCGAPIRepository: PokemonTCGAPIRepository, id: string, onError?: (id: string) => void): Promise<Buffer | undefined> {
     try {
-        const card = await pokemonTCGAPIRepository.card(id)
+        const card = await getCardWithCache(pokemonTCGAPIRepository, id)
         const imageUrl = card?.images?.large
-        const image = await fetch(imageUrl)
-        const imageBuffer = await image.arrayBuffer()
-        return Buffer.from(imageBuffer)
+        return getImageWithCache(imageUrl)
     } catch(error) {
         if (!!onError) {
             onError(id)
@@ -28,12 +59,20 @@ export async function getDeckImages(pokemonTCGAPIRepository: PokemonTCGAPIReposi
         total: cards.length,
         width: 20,
     })
-    const images = cards.map(card => getCardImage(pokemonTCGAPIRepository, card, id => {
-        bar.interrupt(`Error loading ${id}`)
-    }))
-    images.forEach(image => image.then(() => bar.tick()))
-    const resolvedImages = await Promise.all(images)
-    return resolvedImages.filter(image => !!image) as Buffer[]
+
+    const resolvedImages: Buffer[] = []
+
+    for (const card of cards) {
+        const image = await getCardImage(pokemonTCGAPIRepository, card, id => {
+            bar.interrupt(`Error loading ${id}`)
+        })
+        bar.tick()
+        if (!!image) {
+            resolvedImages.push(image)
+        }
+    }
+
+    return resolvedImages
 }
 
 export function createDeck(cards: Card[]): string[] {
@@ -43,8 +82,7 @@ export function createDeck(cards: Card[]): string[] {
 
 export async function getSetCodes(pokemonTCGAPIRepository: PokemonTCGAPIRepository): Promise<SetCodes> {
     const sets = await pokemonTCGAPIRepository.sets()
-    const filteredSets = sets.filter(set => !set.id.includes('tg'))
-    const decklist = filteredSets.reduce((prev, curr) => {
+    const setCodes = sets.reduce((prev, curr) => {
         let ptcgoCode = curr.ptcgoCode
 
         // Temporarily patch in missing ptcgoCode from the API data
@@ -57,7 +95,7 @@ export async function getSetCodes(pokemonTCGAPIRepository: PokemonTCGAPIReposito
         return prev
     }, <SetCodes>{})
 
-    return decklist
+    return setCodes
 }
 
 export function logDeckContents(cards: Card[]): void {
